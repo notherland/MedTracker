1a48dbffc02b7d8decd18317a08d36b17c63f464
shiroks <notherland@ya.ru>
2025-08-26 17:57:58 +0300

    Initial commit

---
+++ src/test/java/com/example/medTracker/MedTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
